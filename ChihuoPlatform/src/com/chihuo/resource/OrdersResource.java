960a795f171a5083b18267e76400de96b8e30171
langyan8973 <liuyan1978@sina.com>
2013-01-23 11:02:05 +0800

    修改根据code获取订单

--- ChihuoPlatform/src/com/chihuo/resource/OrdersResource.java
+++ ChihuoPlatform/src/com/chihuo/resource/OrdersResource.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -102,7 +103,7 @@ public class OrdersResource {
 
 
 	@POST
-	@Path("{code}")
+	@Path("code/{code}")
 //	@RolesAllowed({"USER,OWER,WAITER"})
 	@Produces("application/json; charset=UTF-8")
 	public Response joinOrder(@PathParam("code") String code,
